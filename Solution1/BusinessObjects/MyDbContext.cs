4bc072c5ab7fff7532468b9571452e06666adcab
TrungHa-IT <hatrung03022003@gmail.com>
2025-06-30 11:19:23 +0700

    Initial project

---
+++ Solution1/BusinessObjects/MyDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace BusinessObjects
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfiguration configuration = builder.Build();
+            string connectionString = configuration.GetConnectionString("MyStoreDB");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder optionsBuilder)
+        {
+            optionsBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Beverages" },
+                new Category { CategoryId = 2, CategoryName = "Condiments" },
+                new Category { CategoryId = 3, CategoryName = "Confections" },
+                new Category { CategoryId = 4, CategoryName = "Dairy Products" },
+                new Category { CategoryId = 5, CategoryName = "Grains/Cereals" },
+                new Category { CategoryId = 6, CategoryName = "Meat/Poultry" },
+                new Category { CategoryId = 7, CategoryName = "Produce" },
+                new Category { CategoryId = 8, CategoryName = "Seafood" }
+            );
+        }
+    }
+}
